c0ccf053d5a68e6c91c28a29d0ceabfb5890dd50
Tochukwu Nwachukwu <truetochukz@gmail.com>
2024-11-12 09:27:36 +0200

    Importing defaults

--- chp5/importing-defaults/index.ts
+++ chp5/importing-defaults/index.ts
@@ -1,3 +1,31 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+
+const DefaultSg = new aws.ec2.SecurityGroup(
+  "DefaultSg",
+  {
+    description: "default VPC security group",
+    egress: [
+      {
+        cidrBlocks: ["0.0.0.0/0"],
+        fromPort: 0,
+        protocol: "-1",
+        toPort: 0,
+      },
+    ],
+    ingress: [
+      {
+        fromPort: 0,
+        protocol: "-1",
+        self: true,
+        toPort: 0,
+      },
+    ],
+    name: "default",
+    vpcId: "vpc-097728e00a72ac827",
+  },
+  {
+    protect: true,
+  }
+);
--- chp5/importing-defaults/readme.md
+++ chp5/importing-defaults/readme.md
@@ -7,5 +7,12 @@ The pforject imports default resources such as default security group and securi
 **Importing default security group**
 
 ```
-$ pulumi import aws:ec2/securityGroup:SecurityGroup elb_sg sg-903004f8
+$ pulumi import aws:ec2/securityGroup:SecurityGroup DefaultSg sg-0a08029fea883893f
+```
+
+Copy the generated code into the `index.ts` file and save the file.  
+Run pulumi up
+
+```
+$ pulumi up
 ```
